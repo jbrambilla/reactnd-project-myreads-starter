02a84e70408bb158ea798161967a3cd6085c0402
Jota <joaobrambilla@outlook.com>
2018-03-30 11:04:43 -0300

    feat: Using react-router to display between search and list pages
    Unfortunatelly, I had also to adjust some ident in the production code

--- src/App.js
+++ src/App.js
@@ -3,30 +3,15 @@ import React from 'react'
 import './App.css'
 import SearchBooks from './SearchBooks';
 import ListBooks from './ListBooks';
+import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks
-            closeSearchPage={() => this.setState({ showSearchPage: false })}
-          />
-        ) : (
-          <ListBooks
-            openSearchPage={() => this.setState({ showSearchPage: true })}
-          />
-        )}
+        <Route path="/search" component={SearchBooks}/>
+        <Route exact path="/" component={ListBooks} />
       </div>
     )
   }
