520a4a2889ac3c706fc4d7975972ac953c442f9e
Jota <joaobrambilla@outlook.com>
2018-03-30 11:49:43 -0300

    feat: Using Books API to retrieve the book list
    This feat comes with the creation of the BookShelf component to
    render the book lists.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks';
 import ListBooks from './ListBooks';
@@ -7,11 +7,27 @@ import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
 
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI
+      .getAll()
+      .then(books => (
+        this.setState({books})
+      ));
+  }
+
   render() {
     return (
       <div className="app">
         <Route path="/search" component={SearchBooks}/>
-        <Route exact path="/" component={ListBooks} />
+        <Route exact path="/" render={() => (
+          <ListBooks 
+            books={this.state.books}
+          />
+        )}/>
       </div>
     )
   }
