ddba64e9311ab161dee27be300fd015352ebf594
jbrambilla <joao.brambilla@egestora.com.br>
2018-04-03 11:15:24 -0300

    refactor: Made both BookShelf and ListBooks as functional components

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,40 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
-class BookShelf extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        onChangeShelf: PropTypes.func.isRequired
-    }
+function BookShelf(props) {
+    const {books, onChangeShelf} = props;
 
-    render() {
-        const {books, onChangeShelf} = this.props;
-
-        return (
-            <ol className="books-grid">
-                {books.map(book => (
-                    <li key={book.id}>
-                        <div className="book">
-                        <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select onChange={(e) => onChangeShelf(book, e)} value={book.shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors.join(', ')}</div>
+    return (
+        <ol className="books-grid">
+            {books.map(book => (
+                <li key={book.id}>
+                    <div className="book">
+                    <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select onChange={(e) => onChangeShelf(book, e)} value={book.shelf}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors.join(', ')}</div>
+                </div>
+                </li>
+            ))}
+        </ol>
+    )
+}
+
+BookShelf.propTypes = {
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,51 +1,48 @@
-import React, {Component} from 'react'; 
+import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf';
 
-class ListBooks extends Component
-{
-  static propTypes = {
-    books: PropTypes.object.isRequired,
-    onChangeShelf: PropTypes.func.isRequired
-  }
+function ListBooks(props) {
+  const {books, onChangeShelf} = props;
 
-  render() {
-    const {books, onChangeShelf} = this.props;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Curretly Reading</h2>
-              <div className="bookshelf-books">
-                <BookShelf books={books.currentlyReading} onChangeShelf={onChangeShelf}  />
-              </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Curretly Reading</h2>
+            <div className="bookshelf-books">
+              <BookShelf books={books.currentlyReading} onChangeShelf={onChangeShelf}  />
             </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <BookShelf books={books.wantToRead} onChangeShelf={onChangeShelf}  />
-              </div>
+          </div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Want to Read</h2>
+            <div className="bookshelf-books">
+              <BookShelf books={books.wantToRead} onChangeShelf={onChangeShelf}  />
             </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <BookShelf books={books.read} onChangeShelf={onChangeShelf}  />
-              </div>
+          </div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+            <div className="bookshelf-books">
+              <BookShelf books={books.read} onChangeShelf={onChangeShelf}  />
             </div>
           </div>
         </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
+}
+
+ListBooks.propTypes = {
+  books: PropTypes.object.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks
